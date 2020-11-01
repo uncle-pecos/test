b6ac8863bc3236131e634dd82c719363d733bb95
Oleg <oleg3422@gmail.com>
2020-11-01 20:10:04 +0500

    1

--- test folder/first.py
+++ test folder/first.py
@@ -9,12 +9,32 @@ class Fruit():
 
 class Apple(Fruit):
     color = 'red'
+    col = 0
+
+    def __init__(self, name):   #инициализирует объект
+        print("Apple was made")
+        Apple.col += 1
+        self.name = name
+
+    def __str__(self):      #возвращает строку (в данном случае строку name)
+        return self.name
+
+   # def __repr__(self):
+
 
     def prt(self):
         print ('this is apple with size ' + str(self.size) + ' and color ' + self.color )
 
 
-apl = Apple()
+apl = Apple('a')
 apl.ch_size(2)
-print (apl.size)
+print (apl)
 apl.prt()
+applefarm = [Apple('b'), Apple('c'), Apple('d')]
+#for i in applefarm:
+#    print(i) 
+#print (Apple.col)
+setattr(apl, 'size', 4)
+print (getattr(apl, 'size'))
+delattr(apl, 'name')
+print (hasattr(apl, 'name'))
